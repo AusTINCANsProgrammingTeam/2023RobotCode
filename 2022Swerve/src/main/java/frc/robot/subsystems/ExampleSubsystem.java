9e91910b3bd0408156995f9f9587a22f7db283a3
ModBoyEX <ModBoyEX@gmail.com>
2022-08-25 17:45:41 -0500

    Spotless

--- 2022Swerve/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ 2022Swerve/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -8,7 +8,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public ExampleSubsystem() {
+    
+  }
 
   @Override
   public void periodic() {
