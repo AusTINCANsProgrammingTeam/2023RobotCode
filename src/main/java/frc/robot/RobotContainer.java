ac14e5e200c4076b55f5abcd43311271712fd51e
Adam Vander Pas <adam.vander.pas@ni.com>
2022-08-26 16:50:12 -0500

    Start to setup example junit test. Doesn't seem to run at all yet, don't know the right way to instantiate it.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.tests.ExampleSubsystemTest;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -21,6 +22,8 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  private final ExampleSubsystemTest m_exampleSubsystemTest = new ExampleSubsystemTest();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,11 +4,24 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class ExampleSubsystem extends SubsystemBase implements AutoCloseable {
+
+  private CANSparkMax motor;
+
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public ExampleSubsystem() {
+    motor = new CANSparkMax(0, MotorType.kBrushless); 
+  }
+
+  public void spin(double velocity) {
+    motor.set(velocity);
+  }
+
 
   @Override
   public void periodic() {
@@ -18,5 +31,13 @@ public class ExampleSubsystem extends SubsystemBase {
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+  }
+
+  @Override
+  public void close() throws Exception {
+    // This method will close all device handles used by this object and release any other dynamic memory.
+    // Mostly for JUnit tests
+    motor.close();
+    
   }
 }
