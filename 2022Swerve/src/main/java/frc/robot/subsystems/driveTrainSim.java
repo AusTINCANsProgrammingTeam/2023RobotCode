6de375960c4268fe1df090015e3e92e2b9f6b535
ModBoyEX <ModBoyEX@gmail.com>
2022-08-25 19:20:02 -0500

    Rembering how to simulate

--- 2022Swerve/src/main/java/frc/robot/subsystems/driveTrainSim.java
+++ 2022Swerve/src/main/java/frc/robot/subsystems/driveTrainSim.java
@@ -4,28 +4,103 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class driveTrainSim extends SubsystemBase {
 
+  static final double KvLinear = 1.98;
+  static final double KaLinear = 0.2;
+  static final double KvAngular = 1.5;
+  static final double KaAngular = 0.3;
+
+  private PWMSparkMax m_leftMotor = new PWMSparkMax(0);
+  private PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+
+
   private Encoder m_leftEncoder = new Encoder(0, 1);
   private Encoder m_rightEncoder = new Encoder(2, 3);
-
   private EncoderSim m_leftEncoderSim = new EncoderSim(m_leftEncoder);
   private EncoderSim m_rightEncoderSim = new EncoderSim(m_rightEncoder);
 
-  /** Creates a new ExampleSubsystem. */
-  public driveTrainSim() {}
+  private AnalogGyro m_gyro = new AnalogGyro(1);
+  private AnalogGyroSim m_gyroSim = new AnalogGyroSim(m_gyro);
+
+  private Field2d m_field = new Field2d();
+
+  private SwerveDriveOdometry m_odometry;
+
+
+  DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(
+    // Create a linear system from our identification gains.
+    LinearSystemId.identifyDrivetrainSystem(KvLinear, KaLinear, KvAngular, KaAngular),
+    DCMotor.getNEO(2),       // 2 NEO motors on each side of the drivetrain.
+    7.29,                    // 7.29:1 gearing reduction.
+    0.7112,                  // The track width is 0.7112 meters.
+    Units.inchesToMeters(3), // The robot uses 3" radius wheels.
+  
+    // The standard deviations for measurement noise:
+    // x and y:          0.001 m
+    // heading:          0.001 rad
+    // l and r velocity: 0.1   m/s
+    // l and r position: 0.005 m
+    VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
+
+  public driveTrainSim() {
+    SmartDashboard.putData("Field", m_field);
+    
+    //SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics,
+    //getGyroHeading(), new Pose2d(5.0, 13.5, new Rotation2d()));
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // This will get the simulated sensor readings that we set
+    // in the previous article while in simulation, but will use
+    // real values on the robot itself.
+    
+    // m_odometry.update(m_gyro.getRotation2d(),
+    // m_leftEncoder.getDistance(),
+    // m_rightEncoder.getDistance());
+    // m_field.setRobotPose(m_odometry.getPoseMeters());
   }
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+    // Set the inputs to the system. Note that we need to convert
+    // the [-1, 1] PWM signal to voltage by multiplying it by the
+    // robot controller voltage.
+    m_driveSim.setInputs(m_leftMotor.get() * RobotController.getInputVoltage(),
+    m_rightMotor.get() * RobotController.getInputVoltage());
+
+    // Advance the model by 20 ms. Note that if you are running this
+    // subsystem in a separate thread or have changed the nominal timestep
+    // of TimedRobot, this value needs to match it.
+    m_driveSim.update(0.02);
+
+    // Update all of our sensors.
+    m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
+    m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
+    m_rightEncoderSim.setDistance(m_driveSim.getRightPositionMeters());
+    m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
+    m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
   }
 }
