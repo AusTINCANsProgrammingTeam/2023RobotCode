591564be6381118703ad2e1f1fad7d0141cbd1a1
zaneb <zanebrotherton@gmail.com>
2022-08-26 19:56:40 -0500

    Implement initial SwerveSubsystem class

--- 2022Swerve/src/main/java/frc/robot/Constants.java
+++ 2022Swerve/src/main/java/frc/robot/Constants.java
@@ -16,18 +16,58 @@ import edu.wpi.first.math.util.Units;
  */
 public final class Constants {
 
-    public static final class DriveConstants{
-        public static final double kPhysicalMaxSpeed = 0; //Max drivebase speed in Meters per second
+    public static final class DriveConstants{ //TODO: Update these constants with physical values
+        public static final double kPhysicalMaxSpeed = 0; //Max drivebase speed in meters per second
+
+        public static final class frontLeftModule{
+            public static final int driveMotorID = 0;
+            public static final int turningMotorID = 0;
+            public static final boolean driveMotorReversed = false;
+            public static final boolean turningMotorReversed = false;
+            public static final int absoluteEncoderID = 0;
+            public static final double absoluteEncoderOffset = 0; 
+            public static final boolean absoluteEncoderReversed = false;
+        }
+
+        public static final class frontRightModule{
+            public static final int driveMotorID = 0;
+            public static final int turningMotorID = 0;
+            public static final boolean driveMotorReversed = false;
+            public static final boolean turningMotorReversed = false;
+            public static final int absoluteEncoderID = 0;
+            public static final double absoluteEncoderOffset = 0; 
+            public static final boolean absoluteEncoderReversed = false; 
+        }
+
+        public static final class backLeftModule{
+            public static final int driveMotorID = 0;
+            public static final int turningMotorID = 0;
+            public static final boolean driveMotorReversed = false;
+            public static final boolean turningMotorReversed = false;
+            public static final int absoluteEncoderID = 0;
+            public static final double absoluteEncoderOffset = 0; 
+            public static final boolean absoluteEncoderReversed = false;
+        }
+
+        public static final class backRightModule{
+            public static final int driveMotorID = 0;
+            public static final int turningMotorID = 0;
+            public static final boolean driveMotorReversed = false;
+            public static final boolean turningMotorReversed = false;
+            public static final int absoluteEncoderID = 0;
+            public static final double absoluteEncoderOffset = 0; 
+            public static final boolean absoluteEncoderReversed = false;
+        }
     }
 
     public static final class SwerveModuleConstants{ //TODO: Update these constants with physical values
         public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
         public static final double kDriveMotorGearRatio = 1 / 1;
         public static final double kTurningMotorGearRatio = 1 / 1;
-        public static final double kDriveEncoderRotFactor = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters; //Conversion factor converting the Drive Encoder's Rotations to Meters
-        public static final double kDriveEncoderRPMFactor = kDriveEncoderRotFactor / 60; //Conversion factor converting the Drive Encoder's RPM to Meters per second
-        public static final double kTurningEncoderRotFactor = kTurningMotorGearRatio * 2 * Math.PI; //Conversion factor converting the Turn Encoder's Rotations to Radians
-        public static final double kTurningEncoderRPMFactor = kTurningEncoderRotFactor / 60; //Conersion factor converting the Turn Encoder's RPM to Radians per second
+        public static final double kDriveEncoderRotFactor = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters; //Conversion factor converting the Drive Encoder's rotations to meters
+        public static final double kDriveEncoderRPMFactor = kDriveEncoderRotFactor / 60; //Conversion factor converting the Drive Encoder's RPM to meters per second
+        public static final double kTurningEncoderRotFactor = kTurningMotorGearRatio * 2 * Math.PI; //Conversion factor converting the Turn Encoder's rotations to Radians
+        public static final double kTurningEncoderRPMFactor = kTurningEncoderRotFactor / 60; //Conersion factor converting the Turn Encoder's RPM to radians per second
         public static final double kPTurning = 0; //P gain for the turning motor
     }
 }
