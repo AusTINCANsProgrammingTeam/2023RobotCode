066c1d49f90a2aad87e3ee0b3ec20c904ba0bcc6
zaneb <zanebrotherton@gmail.com>
2022-08-27 20:02:34 -0500

    Implement initial SwerveTeleopCommand class

--- 2022Swerve/src/main/java/frc/robot/Constants.java
+++ 2022Swerve/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 
 /**
@@ -16,8 +18,36 @@ import edu.wpi.first.math.util.Units;
  */
 public final class Constants {
 
+    public static final class OIConstants{
+        //Constants for Operator Interface
+        public static final double kDeadband = 0;
+
+        public static final int kDriverControllerPort = 0;
+
+        public static final int kDriverYAxis = 0;
+        public static final int kDriverXAxis = 0;
+        public static final int kDriverRotAxis = 0;
+
+        public static final int kDriverFODButtonID = 0; //Field Oriented Drive toggle button
+        public static final int kDriveZeroButtonID = 0;
+        
+    }
+
     public static final class DriveConstants{ //TODO: Update these constants with physical values
         public static final double kPhysicalMaxSpeed = 0; //Max drivebase speed in meters per second
+        public static final double kPhysicalMaxAngularSpeed = 0; //Max drivebase angular speed in radians per second
+        public static final double kMaxAcceleration = 0; //Max allowed acceleration in units per second (Not a physical value)
+        public static final double kMaxAngluarAcceleration = 0; //Max allowed angluar acceleration in units per second (Not a physical value)
+        public static final double kSpeedFactor = 4;  //Factor to divide the physical max speed by to use as max speed in mapping
+        public static final double kAngluarSpeedFactor = 4; //Factor to divide the physical max speed by to use as max speed in mapping
+
+        public static final double kTrackWidth = Units.inchesToMeters(0); //Distance between right and left wheels
+        public static final double kWheelBase = Units.inchesToMeters(0); //Distance between front and back wheels
+        public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics( //Creates robot geometry using the locations of the 4 wheels
+            new Translation2d(kWheelBase / 2, -kTrackWidth / 2), 
+            new Translation2d(kWheelBase / 2, kTrackWidth /2),
+            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, kTrackWidth / 2));
 
         public static final class frontLeftModule{
             public static final int driveMotorID = 0;
---
+++ 2022Swerve/src/main/java/frc/robot/commands/SwerveTeleopCommand.java
@@ -0,0 +1,79 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class SwerveTeleopCommand extends CommandBase {
+
+    private final SwerveSubsystem swerveSubsystem;
+    private final Supplier<Double> xSpeedFunction, ySpeedFunction, turnSpeedFunction;
+    private final Supplier<Boolean> fieldOrientedFunction; //Whether or not control is field oriented
+    private final SlewRateLimiter xLimiter, yLimiter, turnLimiter;
+
+    public SwerveTeleopCommand(SwerveSubsystem swerveSubsystem,
+            Supplier<Double> xSpeedFunction, Supplier<Double> ySpeedFunction, Supplier<Double> turnSpeedFunction,
+            Supplier<Boolean> fieldOrientedFunction) {
+        this.swerveSubsystem = swerveSubsystem;
+        this.xSpeedFunction = xSpeedFunction;
+        this.ySpeedFunction = ySpeedFunction;
+        this.turnSpeedFunction = turnSpeedFunction;
+        this.fieldOrientedFunction = fieldOrientedFunction;
+        this.xLimiter = new SlewRateLimiter(DriveConstants.kMaxAcceleration);
+        this.yLimiter = new SlewRateLimiter(DriveConstants.kMaxAcceleration);
+        this.turnLimiter = new SlewRateLimiter(DriveConstants.kMaxAngluarAcceleration);
+        addRequirements(swerveSubsystem);
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {
+        //Get joystick inputs
+        double xSpeed = xSpeedFunction.get();
+        double ySpeed = ySpeedFunction.get();
+        double turnSpeed = turnSpeedFunction.get();
+
+        //Apply deadband
+        xSpeed = Math.abs(xSpeed) > OIConstants.kDeadband ? xSpeed : 0.0;
+        ySpeed = Math.abs(ySpeed) > OIConstants.kDeadband ? ySpeed : 0.0;
+        turnSpeed = Math.abs(turnSpeed) > OIConstants.kDeadband ? turnSpeed : 0.0;
+
+        //Smooth input using a slew limiter, map to a speeds in meters/radians per second
+        xSpeed = xLimiter.calculate(xSpeed) * (DriveConstants.kPhysicalMaxSpeed / DriveConstants.kSpeedFactor);
+        ySpeed = yLimiter.calculate(ySpeed) * (DriveConstants.kPhysicalMaxSpeed / DriveConstants.kSpeedFactor);
+        turnSpeed = turnLimiter.calculate(turnSpeed) * (DriveConstants.kPhysicalMaxSpeed / DriveConstants.kSpeedFactor);
+
+        //Construct Chassis Speeds
+        ChassisSpeeds chassisSpeeds;
+        if(fieldOrientedFunction.get()){
+            //Field Oriented Drive
+            chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, turnSpeed, swerveSubsystem.getRotation2d());
+        } else {
+            //Robot Oriented Drive
+            chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turnSpeed);
+        }
+
+        //Convert Chassis Speeds to individual module states
+        SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+
+        swerveSubsystem.setModuleStates(moduleStates);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        swerveSubsystem.stopModules();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
