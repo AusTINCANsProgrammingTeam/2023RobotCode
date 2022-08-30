303e915129e300493d44c6574e0b6637eaec1153
azvanderpas <azvanderpas@gmail.com>
2022-08-30 12:27:28 -0500

    Features/setup junit and example (#3)
    * Start to setup example junit test. Doesn't seem to run at all yet, don't know the right way to instantiate it.
    
    * JUnit tests need to be under src/test/java directory...
    
    * Give up on trying to do an example JUnit test with CANSparkMax and just use PWMSparkMax instead.
    
    Co-authored-by: Adam Vander Pas <adam.vander.pas@ni.com>

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,11 +4,23 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class ExampleSubsystem extends SubsystemBase implements AutoCloseable {
+
+  // FIXME using PWMSparkMax because CANSparkMax doesn't have an equivalent simulation class
+  // May limit how much we can do in terms of JUnit tests
+  private PWMSparkMax motor;
+
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public ExampleSubsystem() {
+    motor = new PWMSparkMax(0);
+  }
+
+  public void spin(double velocity) {
+    motor.set(velocity);
+  }
 
   @Override
   public void periodic() {
@@ -18,5 +30,13 @@ public class ExampleSubsystem extends SubsystemBase {
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+  }
+
+  @Override
+  public void close() throws Exception {
+    // This method will close all device handles used by this object and release any other dynamic memory.
+    // Mostly for JUnit tests
+    motor.close();
+    
   }
 }
