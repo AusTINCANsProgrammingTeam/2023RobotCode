69236a07f532ecbdf8a3bef1c0a62cf91a45b0b1
ModBoyEX <ModBoyEX@gmail.com>
2022-08-31 19:11:18 -0500

    ctr C

--- 2022Swerve/src/main/java/frc/robot/subsystems/driveTrainSim.java
+++ 2022Swerve/src/main/java/frc/robot/subsystems/driveTrainSim.java
@@ -37,70 +37,46 @@ public class driveTrainSim extends SubsystemBase {
 
   private Encoder m_leftEncoder = new Encoder(0, 1);
   private Encoder m_rightEncoder = new Encoder(2, 3);
-  private EncoderSim m_leftEncoderSim = new EncoderSim(m_leftEncoder);
-  private EncoderSim m_rightEncoderSim = new EncoderSim(m_rightEncoder);
-
-  private AnalogGyro m_gyro = new AnalogGyro(1);
-  private AnalogGyroSim m_gyroSim = new AnalogGyroSim(m_gyro);
+  private EncoderSim m_leftEncoderSim;
+  private EncoderSim m_rightEncoderSim;
+  
+  private AnalogGyro m_gyro1 = new AnalogGyro(1);
+  private AnalogGyroSim m_gyroSim = new AnalogGyroSim(m_gyro1);
 
   private Field2d m_field = new Field2d();
 
-  private SwerveDriveOdometry m_odometry;
+  private DifferentialDriveOdometry m_differentialOdometry;
 
 
-  DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(
-    // Create a linear system from our identification gains.
-    LinearSystemId.identifyDrivetrainSystem(KvLinear, KaLinear, KvAngular, KaAngular),
-    DCMotor.getNEO(2),       // 2 NEO motors on each side of the drivetrain.
-    7.29,                    // 7.29:1 gearing reduction.
-    0.7112,                  // The track width is 0.7112 meters.
-    Units.inchesToMeters(3), // The robot uses 3" radius wheels.
-  
-    // The standard deviations for measurement noise:
-    // x and y:          0.001 m
-    // heading:          0.001 rad
-    // l and r velocity: 0.1   m/s
-    // l and r position: 0.005 m
-    VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
+  private DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(LinearSystemId.identifyDrivetrainSystem(KvLinear, KaLinear, KvAngular, KaAngular),DCMotor.getNEO(2),7.29,0.7112,Units.inchesToMeters(3),VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
 
   public driveTrainSim() {
     SmartDashboard.putData("Field", m_field);
-    
-    //SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics,
-    //getGyroHeading(), new Pose2d(5.0, 13.5, new Rotation2d()));
+
   }
 
   @Override
   public void periodic() {
-    // This will get the simulated sensor readings that we set
-    // in the previous article while in simulation, but will use
-    // real values on the robot itself.
     
-    // m_odometry.update(m_gyro.getRotation2d(),
-    // m_leftEncoder.getDistance(),
-    // m_rightEncoder.getDistance());
-    // m_field.setRobotPose(m_odometry.getPoseMeters());
   }
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-    // Set the inputs to the system. Note that we need to convert
-    // the [-1, 1] PWM signal to voltage by multiplying it by the
-    // robot controller voltage.
-    m_driveSim.setInputs(m_leftMotor.get() * RobotController.getInputVoltage(),
-    m_rightMotor.get() * RobotController.getInputVoltage());
+  
+  m_driveSim.setInputs(m_leftMotor.get() * RobotController.getInputVoltage(),
+  m_rightMotor.get() * RobotController.getInputVoltage());
 
-    // Advance the model by 20 ms. Note that if you are running this
-    // subsystem in a separate thread or have changed the nominal timestep
-    // of TimedRobot, this value needs to match it.
-    m_driveSim.update(0.02);
+  m_driveSim.update(0.02);
 
-    // Update all of our sensors.
-    m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
-    m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
-    m_rightEncoderSim.setDistance(m_driveSim.getRightPositionMeters());
-    m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
-    m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
+  m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
+  m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
+  m_rightEncoderSim.setDistance(m_driveSim.getRightPositionMeters());
+  m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
+  m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
+
+  m_differentialOdometry.update(m_gyro1.getRotation2d(),
+                    m_leftEncoder.getDistance(),
+                    m_rightEncoder.getDistance());
+  m_field.setRobotPose(m_differentialOdometry.getPoseMeters());
   }
 }
