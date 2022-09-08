92a218858d2d4eaf0b858ca31c7a8c8a12e02bd1
ModBoyEX <ModBoyEX@gmail.com>
2022-09-07 19:36:11 -0500

    Changing Joystick Axis numbers

--- 2022Swerve/src/main/java/frc/robot/Constants.java
+++ 2022Swerve/src/main/java/frc/robot/Constants.java
@@ -24,9 +24,9 @@ public final class Constants {
 
         public static final int kDriverControllerPort = 0;
 
-        public static final int kDriverYAxis = 0;
+        public static final int kDriverYAxis = 1;
         public static final int kDriverXAxis = 0;
-        public static final int kDriverRotAxis = 0;
+        public static final int kDriverRotAxis = 2;
 
         public static final int kDriverFODButtonID = 0; //Field Oriented Drive toggle button
         public static final int kDriveZeroButtonID = 0;
