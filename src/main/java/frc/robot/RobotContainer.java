fac8d213f80219632275c9dff949f3040169386a
zaneb <zanebrotherton@gmail.com>
2022-12-08 11:49:18 -0600

    Change simulation subsystem to only instantiate if simulation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,7 +25,7 @@ public class RobotContainer {
   private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
 
   private final AutonSubsytem autonSubsytem = new AutonSubsytem(swerveSubsystem);
-  private final SimulationSubsystem simulationSubsystem = new SimulationSubsystem(swerveSubsystem);
+  private final SimulationSubsystem simulationSubsystem = Robot.isSimulation() ? new SimulationSubsystem(swerveSubsystem) : null;
   
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
