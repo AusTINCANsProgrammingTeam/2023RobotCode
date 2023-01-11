0d7b4077a71d0b029c0d496adf3d7c92ba67c3a1
ModBoyEX <ModBoyEX@gmail.com>
2023-01-11 17:49:05 -0600

    Change for PR

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,9 +18,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-
-
-
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -132,7 +132,7 @@ public class SwerveModule extends SubsystemBase {
     }
 
     public double getDriveVelocity() {
-        return Robot.isSimulation() ? simTurningEncoder.getVelocity() : turningEncoder.getVelocity();
+        return Robot.isSimulation() ? simDriveEncoder.getVelocity() : driveEncoder.getVelocity();
     }
     
     public double getTurningVelocity() {
