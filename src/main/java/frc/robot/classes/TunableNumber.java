d4d6840f886e43f5f62700227b38dfc67baa7ff6
Kennyson Le <kennysonle5.0@gmail.com>
2023-01-18 18:19:13 -0600

    Remove tunable Boolean

--- src/main/java/frc/robot/classes/TunableNumber.java
+++ src/main/java/frc/robot/classes/TunableNumber.java
@@ -13,29 +13,22 @@ import frc.robot.Robot;
 public class TunableNumber extends SubsystemBase{
     private static ShuffleboardTab tuningTab = Shuffleboard.getTab("Tuning");
     private GenericEntry numberEntry;
-    private static GenericEntry istuningMode = tuningTab.add("Tuning Mode", true).getEntry();
     private DataLog datalog = DataLogManager.getLog();
     private DoubleLogEntry numberLog;
-    private String name;
     private double value;
     private Consumer<Double> consumer;
-    private boolean tuningMode;
 
     public TunableNumber(String name, double defaultValue, Consumer<Double> consumer){
-        this.name = name;
         this.value = defaultValue;
         this.consumer = consumer;
         if (!Robot.isCompetition){
-            tuningMode = istuningMode.getBoolean(true);
-            if (tuningMode){
-                numberEntry = tuningTab.add(name, defaultValue).getEntry();
-            }
+            numberEntry = tuningTab.add(name, defaultValue).getEntry();
         }
         numberLog = new DoubleLogEntry(datalog, "/tunables/" + name); 
     }
 
     public void periodic(){
-        if (tuningMode || !Robot.isCompetition){
+        if (!Robot.isCompetition){
             double newValue = numberEntry.getDouble(value);
             if (value != newValue){
                 value = newValue;
