0b0a69c7cbf7613ac7354ff4830263d15cfe3c7a
Ryan Rios <104952320+riversry@users.noreply.github.com>
2023-01-21 15:12:16 -0600

    Video Overlay in CameraSubsystem (#19)
    * Create VideoOverlaySubsystem
    
    * Rename VideoOverlaySubsystem to CameraSubsystem
    
    * Implement pull request changes
    
    * refined method explanation
    
    Co-authored-by: Zachary R <zacharywarnerricheson@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,8 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.CameraSubsystem;
+
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -30,6 +32,8 @@ public class RobotContainer {
   
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  
+  private final CameraSubsystem cameraSubsystem = new CameraSubsystem();
 
   private static BatterySubsystem batterySubsystem;
 
