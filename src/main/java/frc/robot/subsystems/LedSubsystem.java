54d54c97b39f1dd39e3c804262d6deba36e9c1e6
Kennyson Le <kennysonle5.0@gmail.com>
2023-01-25 08:15:47 -0600

    Add to ShuffleBoard

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -9,43 +9,41 @@ import java.util.Map;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class LedSubsystem extends SubsystemBase {
-  private PWM pwm;
-  private final AddressableLEDBuffer buffer;
   // TODO Led parameters and RIO ports
   // TODO Add to ShuffleBoard
+public class LedSubsystem extends SubsystemBase {
+  private final AddressableLEDBuffer buffer;
+  private final AddressableLED leds = new AddressableLED(port);
+  private ShuffleboardTab ledTab = Shuffleboard.getTab("Led");
+  private GenericEntry ledState = ledTab.add("OnOff", false).getEntry();
+  private SimpleWidget ledGamePiece = ledTab.add("Color", true).withWidget(BuiltInWidgets.kBooleanBox).withProperties(Map.of("colorWhenFalse", "Purple", "colorWhenTrue", "Yellow"));
   private static final int length = 1; //Amount of Leds in strip light. Or one if we are using a single light
   private static final int port = 0;
-  private final AddressableLED leds = new AddressableLED(port);
   private static enum LedMode {
     CONE, CUBE;
   }
   private LedMode ledMode = LedMode.CONE;
-  /** Creates a new Leds. */
+  /** Configure AddressableLED */
   public LedSubsystem() {
-    LedDriver(1); //Set channel
     buffer = new AddressableLEDBuffer(length);
+    leds.setBitTiming(1, 1, 1, 1);
     leds.setLength(length);
     leds.setData(buffer);
   }
-  /** Sends to Led blinkin */
+  /** Sends to AddressableLED */
   public void setMode(LedMode mode){
       switch (mode) {
           case CUBE: 
             solid(Color.kPurple);
-            System.out.println("I am Cube");
           break;
           case CONE:
             solid(Color.kYellow);
-            System.out.println("I am Cone");
           break;
       }
       leds.setData(buffer);
@@ -58,24 +56,22 @@ public class LedSubsystem extends SubsystemBase {
     }
   }
 
-  /* Sets Pwm Channel */
-  public void LedDriver(int channel) {
-    pwm = new PWM(channel);
-    pwm.setBounds(1,1, 1, 1, 1); /*TODO Setbounds */
-    pwm.setPeriodMultiplier(PWM.PeriodMultiplier.k1X);
-  }
-
   public void stopLed(){
     leds.stop();
+    ledState.setBoolean(false);
   }
   public void startLed(){
     leds.start();
+    ledState.setBoolean(true);
   }
+
   public void changeGamePiece(){
     if (ledMode == LedMode.CONE){
       ledMode = LedMode.CUBE;
+      ledGamePiece.getEntry().setBoolean(false);
     } else if (ledMode == LedMode.CUBE){
       ledMode = LedMode.CONE;
+      ledGamePiece.getEntry().setBoolean(true);
     }
   }
 
