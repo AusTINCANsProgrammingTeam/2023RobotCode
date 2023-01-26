73ac94ecebffb2018fc4c7fb32b9ed76414c59b4
Kennyson Le <kennysonle5.0@gmail.com>
2023-01-26 12:44:20 -0600

    Set leds to color in matrix

--- src/main/java/frc/robot/subsystems/LedMatrixSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedMatrixSubsystem.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
 public class LedMatrixSubsystem {
     public static Color[] colors = {
@@ -20,16 +22,22 @@ Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kYellow, Color.kYe
 Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kYellow, Color.kYellow, Color.kBlack, Color.kBlack, Color.kYellow, Color.kYellow, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, 
 Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kYellow, Color.kYellow, Color.kBlack, Color.kBlack, Color.kYellow, Color.kYellow, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, 
 Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kYellow, Color.kYellow, Color.kYellow, Color.kYellow, Color.kYellow, Color.kYellow, Color.kYellow, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, 
-Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kBlack, Color.kBlack, Color.kBlack, Color.kYellow, Color.kYellow, Color.kYellow, Color.kBlack, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite};
+Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kBlack, Color.kBlack, Color.kBlack, Color.kYellow, Color.kYellow, Color.kYellow, Color.kBlack, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite, Color.kWhite
+};
+
+private final AddressableLEDBuffer buffer;
+private final AddressableLED leds = new AddressableLED(1);
+
+    public LedMatrixSubsystem() {
+        buffer = new AddressableLEDBuffer(256);
+        leds.setBitTiming(1, 1, 1, 1);
+        leds.setLength(256);
+        leds.setData(buffer);
+    }
 
     public void test(){
         for (int i=0; i<colors.length; i++) { 
-            System.out.println(colors[i]);
+            buffer.setLED(i, colors[i]);
         }
     }
 }
-
-
-/*
- * 
- */
