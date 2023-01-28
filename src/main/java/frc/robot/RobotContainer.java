ee2dd50be33d781e4d4ecbf51b30ce35f105856e
ModBoyEX <ModBoyEX@gmail.com>
2023-01-28 10:17:14 -0600

    Correct Spelling

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -76,7 +76,7 @@ public class RobotContainer {
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
-   * @return the command to run in autonomousd
+   * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
     return auton.getAutonCommand();
