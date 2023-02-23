dd4f7112044b6482ff614a16b46fe7e159e4b45e
zaneb <zanebrotherton@gmail.com>
2023-02-23 10:14:00 -0600

    Reinstate inverted intake

--- src/main/java/frc/robot/hardware/MotorController.java
+++ src/main/java/frc/robot/hardware/MotorController.java
@@ -25,7 +25,7 @@ public class MotorController {
         //Arm motors
         ArmBase1(14, 50, IdleMode.kBrake),
         ArmBase2(13, 50, IdleMode.kBrake),
-        ArmElbow(15, 50, IdleMode.kCoast),  
+        ArmElbow(15, 50),  
         //Intake motors
         IntakeMotor1(4, 40, IdleMode.kBrake),
         IntakeMotor2(5, 40, IdleMode.kBrake),
