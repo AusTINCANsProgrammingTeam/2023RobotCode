bafa1bb495e8263dea7400b7837f5a8bbe57cabe
Spike <austincans.2158@gmail.com>
2023-03-08 17:48:06 -0600

    Configure new offset

--- src/main/java/frc/robot/hardware/AbsoluteEncoder.java
+++ src/main/java/frc/robot/hardware/AbsoluteEncoder.java
@@ -17,7 +17,7 @@ public class AbsoluteEncoder {
         BackLeftModule(3, false, -2.789, -3.911),
         BackRightModule(4, false, -3.995, -4.686),
         //Arm Encoders (REV)
-        ArmBase(1, true, Units.degreesToRadians(-14.2)),
+        ArmBase(1, true, Units.degreesToRadians(43.384)),
         ArmChooChoo(2),
         ArmElbow(0, true, Units.degreesToRadians(233.6));
         
