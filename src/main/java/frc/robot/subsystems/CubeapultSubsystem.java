d160a39882275b18216622a82406f7a8da1741f1
Ryan Rios <riversry@gmail.com>
2023-03-18 14:25:54 -0500

    Change cubeapult from servo to solenoid

--- src/main/java/frc/robot/subsystems/CubeapultSubsystem.java
+++ src/main/java/frc/robot/subsystems/CubeapultSubsystem.java
@@ -4,55 +4,65 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.classes.DebugLog;
 
 public class CubeapultSubsystem extends SubsystemBase {
-  private static final double kStartPosition = 0;
-  private static final double kStoredPosition = 0.5;
-  private static final double kLaunchPosition = 1;
-  private static final int servoID = 0;
-  private Servo servo;
-
-  private DebugLog<Double> actualServoPositionLog = new DebugLog<Double>(0.0, "Actual Servo Position", this);
-  private DebugLog<Double> desiredServoPositionLog = new DebugLog<Double>(0.0, "Desired Servo Position", this);
+  private static final int solenoidID = 0; //TODO: Update ids when plugged in to the robot
+  private static final int pneumaticsID = 0;
+  private Solenoid solenoid;
+  private PneumaticHub pneumatics;
+  private ShuffleboardTab configTab = Shuffleboard.getTab("Config");
+  private DebugLog<Boolean> solenoidStateLog = new DebugLog<Boolean>(false, "Solenoid State", this);
   
   public CubeapultSubsystem() {
-    servo = new Servo(servoID);
-    start();
+    configTab.add(new StartEndCommand(this::activate, this::deactivate, this).withName("Enable Compressor"));
+    solenoid = new Solenoid(PneumaticsModuleType.REVPH, solenoidID);
+    pneumatics = new PneumaticHub(pneumaticsID);
+    retract();
+    deactivate();
   }
 
-  private void start() {
-    servo.set(kStartPosition);
-    desiredServoPositionLog.log(kStartPosition);
+  private void extend() {
+    solenoid.set(true);
+    solenoidStateLog.log(true);
   }
 
-  private void store() {
-    servo.set(kStoredPosition);
-    desiredServoPositionLog.log(kStoredPosition);
+  private void retract() {
+    solenoid.set(false);
+    solenoidStateLog.log(false);
   }
 
-  private void launch() {
-    servo.set(kLaunchPosition);
-    desiredServoPositionLog.log(kLaunchPosition);
+  private void activate() {
+    pneumatics.enableCompressorDigital();
   }
 
-  public Command launchCube() {
+  private void deactivate() {
+    pneumatics.disableCompressor();
+  }
+
+  public Command launch() {
     return new SequentialCommandGroup(
-      new InstantCommand(this::launch),
+      new InstantCommand(this::extend, this),
       new WaitCommand(0.5),
-      new InstantCommand(this::store)
-    );
+      new InstantCommand(this::retract, this)
+    ).withName("Launch");
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    actualServoPositionLog.log(servo.get());
+    SmartDashboard.putData(this);
   }
 }
