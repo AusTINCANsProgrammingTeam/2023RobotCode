7fb85d4452e56a6d5b4e5fca30f29c78332aec84
Spike <austincans.2158@gmail.com>
2023-03-19 12:26:47 -0500

    Remove unused import

--- src/main/java/frc/robot/subsystems/CubeapultSubsystem.java
+++ src/main/java/frc/robot/subsystems/CubeapultSubsystem.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
