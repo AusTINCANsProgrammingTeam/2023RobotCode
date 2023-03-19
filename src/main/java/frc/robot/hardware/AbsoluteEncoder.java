a52850762297c93d3b29f028b2dd3aa20e5204f4
Zane Brotherton <46907447+zbrotherton2158@users.noreply.github.com>
2023-03-19 14:23:27 -0500

    Features/chain arm (#74)
    * Hardware fixes
    
    * Update base IDs
    
    * Testing changes
    
    * Testing changes
    
    * Arm feedforward
    
    * Different PID based on up/down
    
    * More 2 PID value changes
    
    * Change feedforward to use actual angles
    
    * Log components of elbow output
    
    * Tunable up/down pid
    
    * Testing changes
    
    * Update highscore position
    
    * Change to isCompetition
    
    * Add max voltage constant
    
    * remove fixme
    
    ---------
    
    Co-authored-by: Spike <austincans.2158@gmail.com>

--- src/main/java/frc/robot/hardware/AbsoluteEncoder.java
+++ src/main/java/frc/robot/hardware/AbsoluteEncoder.java
@@ -17,9 +17,9 @@ public class AbsoluteEncoder {
         BackLeftModule(3, false, -2.789, -3.911),
         BackRightModule(4, false, -3.995, -4.686),
         //Arm Encoders (REV)
-        ArmBase(1, true, Units.degreesToRadians(43.384)),
+        ArmBase(1, true, Units.degreesToRadians(-75.45)),
         ArmChooChoo(2),
-        ArmElbow(0, true, Units.degreesToRadians(233.6));
+        ArmElbow(0, true, Units.degreesToRadians(-65.134)); //FIXME
         
         private int ID;
         private boolean reversed;
