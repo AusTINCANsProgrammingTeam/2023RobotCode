f0261873a8c1edacf4db49f18d83f80fcbe5e79d
Spike <austincans.2158@gmail.com>
2023-03-24 14:08:29 -0500

    new offsets

--- src/main/java/frc/robot/hardware/AbsoluteEncoder.java
+++ src/main/java/frc/robot/hardware/AbsoluteEncoder.java
@@ -17,9 +17,9 @@ public class AbsoluteEncoder {
         BackLeftModule(3, false, -2.789, -3.911),
         BackRightModule(4, false, -3.995, -4.686),
         //Arm Encoders (REV)
-        ArmBase(1, true, Units.degreesToRadians(-75.38)),
+        ArmBase(1, true, Units.degreesToRadians(-75.3)),
         ArmChooChoo(2),
-        ArmElbow(0, true, Units.degreesToRadians(219.05));
+        ArmElbow(0, true, Units.degreesToRadians(217.33));
         
         private int ID;
         private boolean reversed;
