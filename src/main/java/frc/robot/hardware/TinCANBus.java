52c4a611bed7a394395469dbd6868b2353881b7f
Adam Vander Pas <azvanderpas@gmail.com>
2023-05-04 18:01:34 -0500

    Initial TinCANBus I2C setup

--- src/main/java/frc/robot/hardware/TinCANBus.java
+++ src/main/java/frc/robot/hardware/TinCANBus.java
@@ -23,14 +23,42 @@ public class TinCANBus extends CAN {
              1 -> Set I2C device
              2 -> Send Read I2C 
              3 -> Send Write I2C
-             4 -> Return last I2C read & status
-             5 -> Periodic I2C Read
+             4 -> Get last I2C read
+             5 -> Status
+             6 -> Periodic I2C Read
 
         Device Number/ID: 0-63
     */
 
+    private static int DEV_CONFIG_API_CLASS = 0;
+
+    private static int SET_NEW_DEVICEID = 1;
+
+
+    private static int I2C_BRIDGE_API_CLASS = 1;
+
+    private static int   SET_DEVICE = 1;
+    private static int   READ_I2C = 2;
+    private static int   WRITE_I2C = 3;
+    private static int   GET_READ = 4;
+    private static int   STATUS = 5;
+    private static int   PERIODIC_READ_I2C = 6;
+
     public TinCANBus(int deviceId) {
         super(deviceId);
+    }
+
+    private int getApiID(int apiClass, int apiIndex) {
+        return ((apiClass & 0x003F) << 6) + (apiIndex & 0x0F);
+    }
+
+    public void setI2cDevice(byte i2cAddress) {
+        byte [] packet = {i2cAddress};
+        writePacket(packet, getApiID(I2C_BRIDGE_API_CLASS, SET_DEVICE));
+    }
+
+    public void requestI2cRead(byte regAddress) {
+
     }
 
 
